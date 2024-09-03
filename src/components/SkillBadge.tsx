f8519d0a46d99027145f6f556bb352f97eff693a
andreglock <andreglock@gmail.com>
2024-09-03 22:58:03 +0200

    Update to React 18.

--- src/components/SkillBadge.tsx
+++ src/components/SkillBadge.tsx
@@ -1,30 +1,43 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 interface SkillBadgeProp {
-    skill: string;
-    level: number;
+  skill: string;
+  level: number;
 }
 
-export default function SkillBadge (props: SkillBadgeProp) {
-    const { skill, level } = props;
+export default function SkillBadge(props: SkillBadgeProp) {
+  const { skill, level } = props;
 
-    const solidStars = [];
-    const emptyStars = [];
+  const solidStars = [];
+  const emptyStars = [];
 
-    for (let i = 0; i < level; i++) {
-        solidStars.push(<FontAwesomeIcon icon={faStar} key={Math.floor(Math.random() * 100000)}/>);
-    }
-    for (let i = 0; i < 6 - level; i++) {
-        emptyStars.push(<FontAwesomeIcon icon={farStar} key={Math.floor(Math.random() * 100000)}/>);
-    }
+  for (let i = 0; i < level; i++) {
+    solidStars.push(
+      <FontAwesomeIcon
+        icon={faStar as IconProp}
+        key={Math.floor(Math.random() * 100000)}
+      />
+    );
+  }
+  for (let i = 0; i < 6 - level; i++) {
+    emptyStars.push(
+      <FontAwesomeIcon
+        icon={farStar as IconProp}
+        key={Math.floor(Math.random() * 100000)}
+      />
+    );
+  }
 
-    return <li key={Math.floor(Math.random() * 100000)}>
-        <div key={1}>{skill}</div>
-        <div key={2}>
-            {solidStars.map(i => i)}
-            {emptyStars.map(i => i)}
-        </div>
+  return (
+    <li key={Math.floor(Math.random() * 100000)}>
+      <div key={1}>{skill}</div>
+      <div key={2}>
+        {solidStars.map((i) => i)}
+        {emptyStars.map((i) => i)}
+      </div>
     </li>
+  );
 }
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,30 +1,37 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-import ContactForm from '../components/ContactForm';
-import Modal from '../components/Modal';
-import { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import ContactForm from "../components/ContactForm";
+import Modal from "../components/Modal";
+import { useState } from "react";
 
-export default function Contact () {
-    const [showModal, setShowModal] = useState(false);
-    const [showForm, setShowForm] = useState(true);
+export default function Contact() {
+  const [showModal, setShowModal] = useState(false);
+  const [showForm, setShowForm] = useState(true);
 
-    return <section id="contact">
-        <h2 className="p-2">Contact me</h2>
-        <div className="social p-2 d-flex justify-content-around formPlaceholder">
-            {showModal && <Modal setShow={setShowModal} setShowForm={setShowForm}/>}
-            {showForm && <ContactForm setShowModal={setShowModal} setShow={setShowForm}/>}
+  return (
+    <section id="contact">
+      <h2 className="p-2">Contact me</h2>
+      <div className="social p-2 d-flex justify-content-around formPlaceholder">
+        {showModal && (
+          <Modal setShow={setShowModal} setShowForm={setShowForm} />
+        )}
+        {showForm && (
+          <ContactForm setShowModal={setShowModal} setShow={setShowForm} />
+        )}
+      </div>
+      <div className="social p-2 d-flex justify-content-around">
+        <div>
+          <a href="https://github.com/andreglock" target="blank">
+            <FontAwesomeIcon icon={faGithub as IconProp} />
+          </a>
         </div>
-        <div className="social p-2 d-flex justify-content-around">
-            <div>
-                <a href="https://github.com/andreglock" target="blank">
-                    <FontAwesomeIcon icon={faGithub} />
-                </a>
-            </div>
-            <div>
-                <a href="https://www.linkedin.com/in/andreglock/" target="blank">
-                    <FontAwesomeIcon icon={faLinkedinIn} />
-                </a>
-            </div>
+        <div>
+          <a href="https://www.linkedin.com/in/andreglock/" target="blank">
+            <FontAwesomeIcon icon={faLinkedinIn as IconProp} />
+          </a>
         </div>
+      </div>
     </section>
+  );
 }
