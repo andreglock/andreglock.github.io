fe43ca45195e9a9b83750f5654c5deb0f1353f17
andreglock <andreglock@gmail.com>
2024-09-03 22:31:10 +0200

    Update skills.

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -6,20 +6,22 @@ export default function Skills () {
         <div className="col-lg-6 col-12 d-inline-block align-top">
             <h3 className="p-2">Languages I speak</h3>
             <ul>
-                <SkillBadge skill={'HTML'} level={5} />
-                <SkillBadge skill={'CSS / SCSS'} level={5} />
-                <SkillBadge skill={'JavaScript / TypeScript'} level={5} />
+                <SkillBadge skill={'HTML'} level={6} />
+                <SkillBadge skill={'CSS / SCSS'} level={6} />
+                <SkillBadge skill={'JavaScript / TypeScript'} level={6} />
                 <SkillBadge skill={'Python'} level={4} />
                 <SkillBadge skill={'C'} level={2} />
+                <SkillBadge skill={'Java'} level={2} />
             </ul>
         </div>
         <div className="col-lg-6  col-12 d-inline-block">
             <h3 className="p-2">Technologies</h3>
             <ul>
+                <SkillBadge skill={'Angular'} level={6} />
+                <SkillBadge skill={'Nestjs'} level={6} />
                 <SkillBadge skill={'ReactJS'} level={5} />
-                <SkillBadge skill={'Angular'} level={3} />
+                <SkillBadge skill={'MongoDB'} level={5} />
                 <SkillBadge skill={'ExpressJS'} level={4} />
-                <SkillBadge skill={'MongoDB'} level={4} />
                 <SkillBadge skill={'SQL'} level={3} />
                 <SkillBadge skill={'Git'} level={5} />
             </ul>
